eb29fb9bca65fee5281f6e06b0b9b7e6ad53a9ad
Eric.li <eric.li@penpower.com.tw>
2018-04-13 15:31:27 +0800

    join SDK

--- src/Ecpay.php
+++ src/Ecpay.php
@@ -11,10 +11,12 @@ class Ecpay
      * @var SessionManager
      */
     protected $session;
+
     /**
      * @var Repository
      */
     protected $config;
+
     /**
      * Ecpay constructor.
      * @param SessionManager $session
@@ -25,12 +27,37 @@ class Ecpay
         $this->session = $session;
         $this->config = $config;
     }
+
     /**
      * @param string $msg
      * @return string
      */
-    public function test_rtn($msg = ''){
-        $config_arr = $this->config->get('ecpay.options');
-        return $msg.' <strong>from your custom develop package!</strong>';
+    public function test_rtn($value = '')
+    {
+        return 'test';
+    }
+
+    public function logistics()
+    {
+        $logistics = new \ECPayLogistics();
+
+        $logistics->ServiceURL = config('allpay.ServiceURL');
+        $logistics->HashKey    = config('allpay.HashKey');
+        $logistics->HashIV     = config('allpay.HashIV');
+        $logistics->MerchantID = config('allpay.MerchantID');
+        
+        return $logistics;
+    }
+
+    public function instance()
+    {
+        $instance = new \ECPay_AllInOne();
+
+        $instance->ServiceURL = config('allpay.ServiceURL');
+        $instance->HashKey    = config('allpay.HashKey');
+        $instance->HashIV     = config('allpay.HashIV');
+        $instance->MerchantID = config('allpay.MerchantID');
+
+        return $instance;
     }
 }
--- src/config/ecpay.php
+++ src/config/ecpay.php
@@ -1,4 +1,7 @@
 <?php
 return [
-    'options' => []
+    'ServiceURL' => 'http://payment-stage.ecpay.com.tw/Cashier/AioCheckOut',
+    'HashKey'    => '5294y06JbISpM5x9',
+    'HashIV'     => 'v77hoKGq4kWxNNIS',
+    'MerchantID' => '2000132',
 ];
